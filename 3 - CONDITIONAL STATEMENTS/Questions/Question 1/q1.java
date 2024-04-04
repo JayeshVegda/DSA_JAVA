83b9552e9974970d5ca658434bf3b5e0e148a482
JayeshhVegda <jayeshvegda198@gmail.com>
2024-04-04 18:30:37 +0530

    d2

---
+++ 3 - CONDITIONAL STATEMENTS/Questions/Question 1/q1.java
@@ -0,0 +1,20 @@
+// Write a Java program to get a number from the user and print whether it is positive or negative
+
+import java.util.Scanner;
+
+public class q1 {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Number : ");
+        int num = sc.nextInt();
+        if(num >= 0){
+            System.out.println("Your Number is Positive");
+        }
+        else
+        {
+            System.out.println("Your Number is Negative");
+        }
+    }
+    
+}
---
+++ 3 - CONDITIONAL STATEMENTS/Questions/Question 2/q2.java
@@ -0,0 +1,13 @@
+// Finish the following code so that it prints You have a fever if your temperatureis above 100 and otherwise prints You don't have a fever.
+public class q2 {
+    public static void main(String[] args){
+        double temp = 103.5;
+
+        if (temp > 100){
+            System.out.println("You have a Fever, get some medicine");
+        }
+        else{
+            System.out.println("You dont have any fever");
+        }
+    }
+}
