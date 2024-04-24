410700e3a9262dba46f721d20fa91b0461e72412
JayeshhVegda <jayeshvegda198@gmail.com>
2024-04-24 16:38:45 +0530

    update java

--- Solution.java
+++ Solution.java
@@ -1,25 +1,33 @@
-public class Solution {
-    public static String[] findWords(String[] words) {
-        String sr = "asdfghjkl";
-        String tr = "zxcvbnm";
-        String fr = "qwertyuiop";
-        char[] csr = sr.toCharArray();
-        char[] ctr = tr.toCharArray();
-        char[] cfr = fr.toCharArray();
-        int n = words.length;        
-        int count = 0;
+class Solution {
+    public static char slowestKey(int[] releaseTimes, String keysPressed) {
+        int slowestKeyindex = releaseTimes[0];
+        int index = Integer.MAX_VALUE;
+        int n = releaseTimes.length;
 
+        for(int i = 0; i<n-1; i++){
 
-            for (String string : words) 
-            {
-                
-             
+            System.out.println("Current Slowest Key is " + slowestKeyindex);
+            System.out.println("ith Value : " + keysPressed.charAt(i));
+            System.out.println("i+1 Value : "+  keysPressed.charAt(i + 1));
+            System.out.println("time of ith : "+  releaseTimes[i]);
+            System.out.println("time of i+1th : "+  releaseTimes[i+1]);
+            System.out.println("Diff : "+  (releaseTimes[i+1] - releaseTimes[i]));
+            System.out.println();
+
+            int temp = (releaseTimes[i+1] - releaseTimes[i]);
+            if((slowestKeyindex<= temp )){
+                slowestKeyindex = releaseTimes[i+1]-releaseTimes[i];
+                index = i;
             }
-        
-        return words;
+        }
+        return keysPressed.charAt(index);
     }
-    public static void main(String [] args){
-        String words[] = {"Hello","Alaska","Dad","Peace"};
-        findWords(words);
+
+    public static void main(String[] args) {
+        int releaseTimes[] = {9,29,49,50};
+        String str = "cbcd";
+
+        System.out.println(slowestKey(releaseTimes, str));
+
     }
 }
