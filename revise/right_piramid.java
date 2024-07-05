0def1c70e2fe00552bc2db5d45c74296b7b74200
JayeshhVegda <jayeshvegda198@gmail.com>
2024-07-05 11:05:20 +0530

    as

---
+++ revise/right_piramid.java
@@ -0,0 +1,17 @@
+public class right_piramid {
+
+
+    public static  void main(string args[]){
+        // left piramid
+
+        int tar = 4;
+        int counter = 0;
+        for(int i = 0; i<=tar; i++){
+            for(int j = 0; j<tar - 1; j++){
+                System.out.println(" ");
+            }
+
+            for(int k = tar - 1; )
+        }
+    }
+}
