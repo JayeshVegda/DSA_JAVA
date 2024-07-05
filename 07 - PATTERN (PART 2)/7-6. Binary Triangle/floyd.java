8c5a74af89a1eb2dbb552ea8a86d60fca135dfa1
JayeshhVegda <jayeshvegda198@gmail.com>
2024-07-05 11:33:20 +0530

    Update floyd.java

--- 07 - PATTERN (PART 2)/7-6. Binary Triangle/floyd.java
+++ 07 - PATTERN (PART 2)/7-6. Binary Triangle/floyd.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class floyd {
     public static void pat(int n){
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j<=i; j++){
-                if(((i+j)%2) != 0){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j<i; j++){
+                if(((i+j)%2) != 0)
+                {
                     System.out.print("0");
-                }else{
+                }
+                else
+                {
                     System.out.print(1);
                 }
             }
