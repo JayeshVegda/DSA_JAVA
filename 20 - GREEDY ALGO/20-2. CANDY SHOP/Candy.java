3ad651fb375a7a7fcda7406a7a9c4113cad22ff2
JayeshhVegda <jayeshvegda198@gmail.com>
2024-07-22 19:14:54 +0530

    create candy

---
+++ 20 - GREEDY ALGO/20-2. CANDY SHOP/Candy.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Candy {
+    public int shop(int[] candy, int n, int k){
+        int min = 0;
+        Arrays.sort(candy);
+
+        
+
+
+        return min;
+    }
+    public static void main(String[] args) {
+        int candies[] = {3, 2, 1, 4};
+        Candy cd = new Candy();
+        cd.shop(candies, 4, 2);
+    }
+}
